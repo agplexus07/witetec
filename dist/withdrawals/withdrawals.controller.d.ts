9e674032b348c4f3ad2164950633dcc277d15e7b
agplexus07 <agplexus@gmail.com>
2025-03-16 12:04:03 -0300

    Delete dist directory

--- dist/withdrawals/withdrawals.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { WithdrawalsService } from './withdrawals.service';
-import { CreateWithdrawalDto, UpdateWithdrawalStatusDto } from './dto/withdrawal.dto';
-export declare class WithdrawalsController {
-    private readonly withdrawalsService;
-    constructor(withdrawalsService: WithdrawalsService);
-    createWithdrawal(data: CreateWithdrawalDto): Promise<any>;
-    updateStatus(id: string, data: UpdateWithdrawalStatusDto): Promise<any>;
-    getMerchantWithdrawals(merchantId: string): Promise<any[]>;
-}
